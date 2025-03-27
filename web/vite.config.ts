d1075ecd9c1516c3aacaae799c38b4e675d14e0e
developeromprakashalle <alleauh@gmail.com>
2025-03-27 11:57:56 +0400

    added config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Allows access from Docker
+    port: 8080        // Change port from 5173 to 8080
+  }
 })
