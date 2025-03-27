9411823a090f6addd2e37f9796cb95ad2dd856c3
developeromprakashalle <alleauh@gmail.com>
2025-03-27 12:22:19 +0400

    Change port from 5173 to 8080

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',  // Allows access from Docker
-    port: 8080        // Change port from 5173 to 8080
+    port: 8080      // Change port from 5173 to 8080
   }
 })
